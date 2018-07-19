1b0859245962cf11581fc7d67f3e8d0b83323b2f
emerald <ach.aayush.111@gmail.com>
2018-07-19 22:15:39 +0545

    simple arrangrmrnt

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// checked 11
+import React from 'react';
+import ReactDOM from 'react-dom';
+// create a new component. It should produce new components
+const App=()=>{
+    return <div> hello </div>
+}
+ReactDOM.render(<App />, document.querySelector('.container'));
