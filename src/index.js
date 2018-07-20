6e6de80cbc1b20dfe8c21b759de354acdd77db44
emerald <ach.aayush.111@gmail.com>
2018-07-20 21:43:03 +0545

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-// checked 11
+// checked 13
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from 'search_bar';
 // create a new component. It should produce new components
+const API_KEY ='AIzaSyBF5StKJDolrs0UJCqE2k5aJj-o5uncdCA';
 const App=()=>{
     return <div> hello </div>
 }
