7c098149c9f1a3d3aa2fdf929bd8f0fcb5fedcdf
emerald <ach.aayush.111@gmail.com>
2018-07-21 21:59:58 +0545

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 // create a new component. It should produce new components
-const API_KEY ='AIzaSyBF5StKJDolrs0UJCqE2k5aJj-o5uncdCA';
+
 const App=()=>{
      return (<div> 
       <SearchBar />
