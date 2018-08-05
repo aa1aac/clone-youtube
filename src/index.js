36d1c9459f28c9613b38ae63eccdcbc66cd2b9d4
unknown <ach.aayush.111@gmail.com>
2018-08-05 22:37:02 +0545

    update

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-
+import YTSearch from './youtube-search-api';
 const App=()=>{
      return (<div> 
       <SearchBar />
      </div>
 );
-}
+YTSearch({ key:api.API_KEY, term: 'board'}, function(data){
+      console.log(data);
+    });
+  
+//YTsearch({key:api.API_KEY, term:'surfboards'},(data)=>{console.log(data)});
 ReactDOM.render(<App />, document.querySelector('.container'));
