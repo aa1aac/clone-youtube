564448011d932fd2edfc920c29e5f73a3ea93730
aa1aac <ach.aayush.111@gmail.com>
2018-08-08 22:42:09 +0545

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 // checked 20
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-import YTSearch from './youtube-search-api';
+import YTSearch from 'youtube-api-search';
 const App=()=>{
      return (<div> 
       <SearchBar />
      </div>
-);
-YTSearch({ key:api.API_KEY, term: 'board'}, function(data){
-      console.log(data);
-    });
-  
-//YTsearch({key:api.API_KEY, term:'surfboards'},(data)=>{console.log(data)});
+); 
+YTSearch({key:api.API_KEY, term:'surfboards'},(data)=>{console.log(data)});
 ReactDOM.render(<App />, document.querySelector('.container'));
