89ed72a42719b2d874958f8aa3f10a1eb479f61c
aa1aac <ach.aayush.111@gmail.com>
2018-08-11 13:16:42 +0545

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+// checked 25
 import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './component/search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './component/video_list';
 let api='AIzaSyBF5StKJDolrs0UJCqE2k5aJj-o5uncdCA';
-YTSearch({key:api, term:'surfboards'},(data)=>{console.log(data)});
 class App extends Component {
+  constructor(props){
+    super (props);
+    this.state={videos:[]};
+    YTSearch({key:api, term:'surfboard'},(videos)=>{
+      this.setState=({ videos });
+    });
+    
+
+  }
   render() {
     return (
       <div className="App">
         <SearchBar />
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
